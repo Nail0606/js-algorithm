99e6d642efc242c0eeac9cb2ab15b196c9851848
Nail0606 <c9010912@gmail.com>
2023-03-11 23:44:09 +0900

    230311

---
+++ 단계별로 풀어보기/10430.js
@@ -0,0 +1,13 @@
+// (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+// (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+// 세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+let A = parseInt(input[0]);
+let B = parseInt(input[1]);
+let C = parseInt(input[2]);
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
