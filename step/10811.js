693fc0b82904821e113a8f431ee2e81c0bf611c3
nail-notebook <c9010912@gmail.com>
2023-04-03 14:03:38 +0900

    230403

---
+++ step/10811.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+let input;
+try{
+  input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+}catch{
+  input = `5 4
+1 2
+3 4
+1 4
+2 2`.toString().trim().split("\n");
+}
+let N = +(input[0].split(" ")[0]);
+let M = +(input[0].split(" ")[1]);
+let arr = [];
+for(let i = 0; i< N; i++){
+  arr[i] = i + 1;
+}
+for(let i = 0; i < M; i++){
+  let I = +(input[i + 1].split(" ")[0]);
+  let J = +(input[i + 1].split(" ")[1]);
+  let range = arr.slice(I -1 ,J).reverse();
+  arr.splice(I -1, range.length, ...range);
+  
+}
+let answer = arr.join(" ");
+console.log(answer);
