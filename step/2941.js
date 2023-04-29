8f65720e2a93aa554ec792b0780992abb20d1046
ChoMinGi <c9010912@gmail.com>
2023-04-29 23:48:15 +0900

    230429

--- step/2941.js
+++ step/2941.js
@@ -6,15 +6,14 @@ try {
   input = `nljj`.trim();
 }
 
-let croatiaAlpabet = ["c=","c-","dz=","d-","lj","nj","s=","z="];
-let Arr = input
+let croatiaAlpabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
+let Arr = input;
 let count = 0;
-for(let i = 0; i<croatiaAlpabet.length; i++){
-  if(Arr.indexOf(croatiaAlpabet[i]) !== -1){
-    while(Arr.indexOf(croatiaAlpabet[i]) !== -1){
-    Arr = Arr.replace(croatiaAlpabet[i],'*');
-    count++;
-    //지금 lj 를 빼니까 nj 가 남아서 그게 또 빼지는 문제
+for (let i = 0; i < croatiaAlpabet.length; i++) {
+  if (Arr.indexOf(croatiaAlpabet[i]) !== -1) {
+    while (Arr.indexOf(croatiaAlpabet[i]) !== -1) {
+      Arr = Arr.replace(croatiaAlpabet[i], "*");
+      count++;
     }
   }
 }
