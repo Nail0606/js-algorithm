54e20a75b408996d672a4bd08900b76d5ff4de98
ChoMinGi <c9010912@gmail.com>
2023-05-09 23:54:09 +0900

    230509-수정

--- step/2745.js
+++ step/2745.js
@@ -3,10 +3,28 @@ let input;
 try {
   input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
 } catch {
-  input = `ZZZZZ 36`.trim().split(" ");
+  input = `99 36`.trim().split(" ");
+}
+
+let dict = [];
+let k = 65;
+for (let i = 10; i < 36; i++) {
+  dict[String.fromCharCode(k)] = i;
+  k = k + 1;
 }
 
 let B = Number(input[1]);
 let N = input[0];
+let j = N.length;
+let sum = 0;
 
-console.log(parseInt(N, B));
+for (let i = 0; i < N.length; i++) {
+  if (!+N[i]) {
+    j--;
+    sum += dict[N[i]] * B ** j;
+  } else {
+    j--;
+    sum += Number(N[i]) * B ** j;
+  }
+}
+console.log(sum);
