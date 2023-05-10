16b6670199f8bed042ec7700b944cb85016e5428
ChoMinGi <c9010912@gmail.com>
2023-05-11 01:16:36 +0900

    230510

--- step/11005.js
+++ step/11005.js
@@ -3,25 +3,27 @@ let input;
 try {
   input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
 } catch {
-  input = `600 32`
-    .trim()
-    .split(" ");
+  input = `9 36`.trim().split(" ");
 }
-
 let N = +input[0];
 let B = +input[1];
 let C = 0;
 let cnt = 0;
 let array = [];
-while(N !== 0){
+let array2 = [];
+while (N !== 0) {
   M = parseInt(N / B);
-  C = parseInt(N % B)
+  C = parseInt(N % B);
   cnt++;
   N = parseInt(N / B);
-  console.log(cnt, M, C);
-  //array에 실행 결과 나머지들만 담기(push)
+  array.push(C);
 }
 
-for(let i = 0; i< cnt; i++){
-
+for (let i = cnt; i > 0; i--) {
+  if (array[i - 1] > 9) {
+    array2.push(String.fromCharCode(array[i - 1] + 55));
+  } else {
+    array2.push(array[i - 1]);
+  }
 }
+console.log(array2.join(""));
