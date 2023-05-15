abae6c3057f4d961a621417b5b4cc18073f88f40
nail-notebook <c9010912@gmail.com>
2023-05-15 13:27:46 +0900

    230515

--- step/2292.js
+++ step/2292.js
@@ -6,25 +6,22 @@ try {
   input = `21`.trim();
 }
 
-let six = 6;
-let count = 0;
-while (input - 6 >= 0) {
-  input -= six;
-  count++;
-  six += 6;
+let count = 1;
+let first = 2;
+let last = 7;
+let flag = 0;
+
+if(+input === 1){
+  console.log("1");
+}else{
+  while(flag === 0){
+    if(first <= +input  && +input <= last){
+      console.log(count + 1);
+      flag = 1;
+    }else{
+      first = first + 6 * count;
+      last = last + 6 * (count + 1);
+      count++;
+    }
+  }
 }
-console.log(count + 1);
-
-/*
-1
-2 3 4 5 6 7
-8 9 10 11 12 13 14 15 16 17 18 19 
-20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37
-38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61
-
-
-6의 배수로 증가
-
-하나의 계층이 증가할때마다 
-지나가야 할 방의 갯수 + 1
-*/
