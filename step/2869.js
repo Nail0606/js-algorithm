2249e615a589ceecc2cb30f324cc8babfe932d0d
ChoMinGi <c9010912@gmail.com>
2023-05-18 22:43:16 +0900

    230518

--- step/2869.js
+++ step/2869.js
@@ -3,5 +3,13 @@ let input;
 try {
   input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
 } catch {
-  input = `2 1 5`.trim().split(" ");
+  input = `100 99 1000000000`.trim().split(" ");
 }
+
+let up = +input[0];
+let down = +input[1];
+let height = +input[2];
+
+height -= up;
+
+console.log(Math.ceil(height / (up - down)) + 1);
